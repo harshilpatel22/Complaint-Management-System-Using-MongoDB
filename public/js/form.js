bd6b8cb08a83f3a3f3b315410de60e3c829f8847
Daniel Tucker <daniel.tucker@outlook.com>
2017-08-25 22:05:29 -0700

    fixed another borked folder, (thanks onedrive) and updated some basic styling

--- public/js/form.js
+++ public/js/form.js
@@ -1,1 +1,9 @@
+//Set array of review sites you'd like to use.
+var reviewSites = [
+    "https://www.someurl.com"
+]
+
+//Pull in any varialbes passed to this page
 var curLocation = window.location.href;
+
+//Button actions
