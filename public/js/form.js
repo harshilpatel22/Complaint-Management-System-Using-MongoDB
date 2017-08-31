47139dc3d721a65fe6f2b13e67c44151b82ac6c5
Daniel Tucker <daniel.tucker@outlook.com>
2017-08-30 21:23:22 -0700

    added basic theme settings.  Added basic js configuration for logo and url.  Added js to inject logo into header.

--- public/js/form.js
+++ public/js/form.js
@@ -1,19 +1,26 @@
 //Set array of review sites you'd like to use.
-var reviewSites = [
-    "https://www.google.com/search?q=SK+Motorsport,+1999+Iowa+St,+Bellingham,+WA+98229&ludocid=2572613230962149428#lrd=0x5485a38bea214067:0x23b3c21a6ff80834,3"
-]
+smplReview = {
+    reviewSites: [
+        "https://www.google.com/search?q=SK+Motorsport,+1999+Iowa+St,+Bellingham,+WA+98229&ludocid=2572613230962149428#lrd=0x5485a38bea214067:0x23b3c21a6ff80834,3"
+    ],
+    clientLogo: "https://www.skmotorsport.com/wp-content/themes/skmotorsport2/images/skmotorsportlogo.png",
+    clientUrl: "https://www.skmotorsport.com"
+}
+
+//Page styling
+document.getElementById("header").innerHTML = "<a href='" + smplReview.clientUrl + "'><img src='" + smplReview.clientLogo + "' /></a>";
 
 //Pull in any varialbes passed to this page
 var curLocation = window.location.href;
 
 //Button actions
 document.getElementsByClassName("good")[0].onclick = function(){
-    randMax = reviewSites.length;
+    randMax = smplReview.reviewSites.length;
 
     //Remember the reviewSites object starts at 0 so we're not adding anything to this like you might normally do.
     randNum = Math.floor(Math.random() * randMax);
 
-    document.location = reviewSites[randNum];
+    document.location = smplReview.reviewSites[randNum];
 };
 
 document.getElementsByClassName("bad")[0].onclick = function(){
