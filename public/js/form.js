ec843de340ed386d9e0263bac45b5ec1c6b92036
Daniel Tucker <daniel.tucker@outlook.com>
2017-08-31 22:20:47 -0700

    Added ability to pull get variables from url and add them to the smplReview.variables object

--- public/js/form.js
+++ public/js/form.js
@@ -4,14 +4,24 @@ smplReview = {
         "https://www.google.com/search?q=SK+Motorsport,+1999+Iowa+St,+Bellingham,+WA+98229&ludocid=2572613230962149428#lrd=0x5485a38bea214067:0x23b3c21a6ff80834,3"
     ],
     clientLogo: "https://www.skmotorsport.com/wp-content/themes/skmotorsport2/images/skmotorsportlogo.png",
-    clientUrl: "https://www.skmotorsport.com"
+    clientUrl: "https://www.skmotorsport.com",
+    variables: {
+    }
 }
 
 //Page styling
 document.getElementById("header").innerHTML = "<a href='" + smplReview.clientUrl + "'><img src='" + smplReview.clientLogo + "' /></a>";
 
 //Pull in any varialbes passed to this page
-var curLocation = window.location.href;
+(function(){   
+    var parseURI = window.location.href.split(/[?]/g)[1];
+    parseURI = parseURI.split("&");
+
+    for (i=0; i <= parseURI.length; i++) {
+        var tempVar = parseURI[i].split(/[=]/g);
+        smplReview.variables[tempVar[0]] = tempVar[1];
+    }
+})();
 
 //Button actions
 document.getElementsByClassName("good")[0].onclick = function(){
