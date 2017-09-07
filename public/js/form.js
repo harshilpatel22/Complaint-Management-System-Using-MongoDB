4f7ce47417c89ed37ba5b5138c3a37b8c0481ddd
Daniel Tucker <daniel.tucker@outlook.com>
2017-09-06 19:34:56 -0700

    Moved functions around to remove race conditions with buttons and onclick events.

--- public/js/form.js
+++ public/js/form.js
@@ -7,22 +7,51 @@ smplReview = {
     clientUrl: "https://www.skmotorsport.com",
     variables: {
     },
-    ratingFloor: 4
+    ratingFloor: 4,
+    ratingSendToForm: true,
+    ratingStars: false
 }
 
 //Page styling
 document.getElementById("header").innerHTML = "<a href='" + smplReview.clientUrl + "'><img src='" + smplReview.clientLogo + "' /></a>";
 
+var reviewAction = function(type) {
+    if (type === "good") {
+        randMax = smplReview.reviewSites.length;
+    
+        //Remember the reviewSites object starts at 0 so we're not adding anything to this like you might normally do.
+        randNum = Math.floor(Math.random() * randMax);
+    
+        document.location = smplReview.reviewSites[randNum];
+    } else {
+        document.getElementById("container").innerHTML = "<h1>We're sorry to hear that!</h1><div id='sorry'>Please tell us what we can do better:</div>";
+    
+        var sorry = document.getElementById("sorry");
+    
+        sorry.innerHTML = sorry.innerHTML + "<div><textarea name='review' class='form-control' placeholder='Enter you review here...' rows='5'></textarea></div>";
+    };
+};
+
+//Button actions
+document.getElementsByClassName("good")[0].onclick = function(){
+    reviewAction("good");
+};
+document.getElementsByClassName("bad")[0].onclick =  function(){
+    reviewAction("bad");
+};
+
 //Function to handle reviews.
 var reviewHandler = function() {
     if (smplReview.variables.rating != undefined && smplReview.ratingFloor > smplReview.variables.rating) {
-        console.log("poor review");
+        if (smplReview.ratingSendToForm === true) {
+            reviewAction("bad");
+        }
     } else {
         console.log("good review");
     }
 };
 
-//Pull in any variables passed to this page
+//Pull in any variables passed to this page via this anonymous function
 (function(){   
     var parseURI = window.location.href.split(/[?]/g)[1];
 
@@ -33,25 +62,7 @@ var reviewHandler = function() {
             var tempVar = parseURI[i].split(/[=]/g);
             smplReview.variables[tempVar[0]] = tempVar[1];
         }
-        
-        reviewHandler();
     };
+        
+    reviewHandler();
 })();
-
-//Button actions
-document.getElementsByClassName("good")[0].onclick = function(){
-    randMax = smplReview.reviewSites.length;
-
-    //Remember the reviewSites object starts at 0 so we're not adding anything to this like you might normally do.
-    randNum = Math.floor(Math.random() * randMax);
-
-    document.location = smplReview.reviewSites[randNum];
-};
-
-document.getElementsByClassName("bad")[0].onclick = function(){
-    document.getElementById("container").innerHTML = "<h1>We're sorry to hear that!</h1><div id='sorry'>Please tell us what we can do better:</div>";
-
-    var sorry = document.getElementById("sorry");
-
-    sorry.innerHTML = sorry.innerHTML + "<div><textarea name='review' class='form-control' placeholder='Enter you review here...' rows='5'></textarea></div>";
-};
